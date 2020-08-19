51e18bb6996894ab0757dd0e8cd54c207aba233a
Ubuntu <ubuntu@ip-172-31-34-76.eu-west-1.compute.internal>
2020-08-19 14:44:04 +0000

    vpc v1

--- main.tf
+++ main.tf
@@ -3,9 +3,76 @@ provider "aws" {
   shared_credentials_file = "~/.aws/credentials"
 }
 
-resource "aws_instance" "web" {
-  ami           = var.ami
-  instance_type = var.instance
-  key_name      = var.key_name
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.cidr_block
+  enable_dns_hostnames = "true"
+  tags = {
+    Name = "VPC190820"
+  }
 }
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "subnet_a" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.sn_cidr_block
+  availability_zone = data.aws_availability_zones.available.names[0]
+}
+
+resource "aws_instance" "web" {
+  ami                         = var.ami
+  instance_type               = var.instance
+  key_name                    = var.key_name
+  subnet_id                   = aws_subnet.subnet_a.id
+  associate_public_ip_address = true
+  vpc_security_group_ids = ["${aws_security_group.allow_tls.id}"]
+}
+
+resource "aws_route_table" "route" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet_a.id
+  route_table_id = aws_route_table.route.id
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.vpc.id  
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
